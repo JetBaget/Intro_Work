95b9e6f53778ecfafe8da3bac73ef6cda821bbe2
Khromov Anton <jetneitrino@gmail.com>
2016-08-17 23:18:54 +0300

    Version with functions

--- open_url.py
+++ open_url.py
@@ -2,26 +2,55 @@ import requests
 from subprocess import Popen, PIPE
 import logging
 
-call_param = {'aim':'python_developer_job'} #set parameter for GET-inquiry
-response = requests.get('http://gptl.ru/interview_task/api/commands',
-                                                 params = call_param) #calling GET-inquiry
-input_data = response.json()['data'] #extract useful data from response
+URL = 'http://gptl.ru/interview_task/api/commands'
+param = {'aim':'python_developer_job'} 
 
-new_proc = Popen( input_data, shell = True, stdout = PIPE, stderr = PIPE ) #create subprocess
-new_proc.wait() #waiting for subprocess execution
-res = new_proc.communicate() #get tuple with stdout and stderr
+def get_call (url, call_param):
+    #calling GET-inquiry
+    response = requests.get(url, params = call_param)
+    #extract useful data from response
+    in_data = response.json()['data']
+    return in_data
 
-if input_data != '' and res[0] == '': #case of incorrect command processing
-    output_data = res[1]
+def create_proc (func):
+    #create subprocess
+    new_proc = Popen( func, shell = True, stdout = PIPE, stderr = PIPE )
+    #waiting for subprocess execution
+    new_proc.wait()
+    #get tuple with stdout and stderr
+    res = new_proc.communicate()
+    return res
+
+def command_check (res):
+    #case of incorrect command processing
+    if res[1] != '':
+        out_data = res[1]
+    else:
+        out_data = res[0]
+    return out_data
+
+def logger (out_data):
+    #set logging config
+    logging.basicConfig (format='%(message)s', filename='logfile.log',
+                                                  level = logging.INFO)
+    #except 'urllib3' from log
+    logging.getLogger ("urllib3").setLevel (logging.WARNING)
+    #add results in log-file
+    logging.info (out_data)
+
+def post_call (url, in_data, out_data):
+    #data for sending to the server
+    payload = {'command' : in_data, 'result' : out_data }
+    #call POST-inquiry
+    push = requests.post (url, data = payload) 
+
+
+data = get_call (URL, param)
+if data == '':
+    to_log = '! No command from server !\n'
+    logger (to_log)
 else:
-    output_data = res[0]
-
-logging.basicConfig (format='%(message)s', filename='logfile.log',
-                                              level = logging.INFO) #set logging config
-logging.getLogger ("urllib3").setLevel (logging.WARNING) #except 'urllib3' from log
-logging.info (output_data) #add results in log-file
-
-payload = {'command' : input_data, 'result' : output_data } #data for sending to the server
-push = requests.post ('http://gptl.ru/interview_task/api/commands',
-                                                      data = payload) #call POST-inquiry
-
+    com_res = create_proc (data)
+    to_log = command_check (com_res)
+    logger (to_log)
+    post_call (URL, data, to_log)
